d0c54e266bb4744c1c471f68377c6141ef2fe708
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-29 13:31:39 -0300

    feat(app): create state design
    place as props to map, setPlace as props to LocationSearch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import LocationSearch from "./components/LocationSearch";
 import Map from "./components/Map";
 
 export default function App() {
+  const [place, setPlace] = useState<Place | null>(null);
+
   return (
     <div className="h-screen w-screen grid grid-cols-12">
       <div className=" col-span-3 p-2">
-        <LocationSearch />
+        <LocationSearch onLocationClick={(place) => setPlace(place)} />
       </div>
       <div className="col-span-9">
-        <Map />
+        <Map place={place} />
       </div>
     </div>
   );
