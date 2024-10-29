e78123cfed0e0c9cac97886a2076bcd76f8081d9
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-29 16:47:19 -0300

    feat(map): create responsive leaflet map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,3 +1,31 @@
-export default function Map() {
-  return <div>Map</div>;
+import "leaflet/dist/leaflet.css";
+import type { Map as LeafletMap } from "leaflet";
+import { useEffect, useRef } from "react";
+import { MapContainer, TileLayer, Marker } from "react-leaflet";
+
+interface MapProps {
+  place: Place | null;
+}
+
+export default function Map({ place }: MapProps) {
+  const mapRef = useRef<LeafletMap | null>(null);
+
+  useEffect(() => {
+    if (place && mapRef.current) {
+      mapRef.current.flyTo([place.latitude, place.longitude]);
+    }
+  }, [place]);
+
+  return (
+    <MapContainer
+      ref={mapRef}
+      center={[-22.908333, -43.196388]}
+      scrollWheelZoom
+      zoom={12}
+      className="h-full"
+    >
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {place && <Marker position={[place.latitude, place.longitude]} />}
+    </MapContainer>
+  );
 }
