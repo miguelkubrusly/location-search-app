a0bedd119bfe27d7c44701473c30b9ba7fd0e3db
Miguel Kubrusly <125589621+miguelkubrusly@users.noreply.github.com>
2024-10-28 21:28:31 -0300

    chore(global): reinstall npm and 1st boilerplates

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import LocationSearch from "./components/LocationSearch";
+import Map from "./components/Map";
+
+export default function App() {
+  return (
+    <div className="h-screen w-screen grid grid-cols-12">
+      <div className=" col-span-3 p-2">
+        <LocationSearch />
+      </div>
+      <div className="col-span-9">
+        <Map />
+      </div>
+    </div>
+  );
+}
